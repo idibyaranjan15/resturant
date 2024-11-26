48e37f5151cb50468a11d2894cba61d34dfabb89
idibyaranjan15 <dibyaranjansahoo696@gmail.com>
2024-11-26 13:03:35 +0530

    Todo using Recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Todos from "./components/Todos";
+import { RecoilRoot } from "recoil";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <RecoilRoot>
+      <div className="min-h-screen bg-gray-100 flex  justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold text-gray-800 mb-8">
+            Welcome to Todo App
+          </h1>
+          <Todos />
+        </div>
+      </div>
+    </RecoilRoot>
+  );
 };
 
 export default App;
