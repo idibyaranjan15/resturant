4bf7cf6232e047275af05508ad7213ca3902fb60
idibyaranjan15 <dibyaranjansahoo696@gmail.com>
2024-12-02 11:14:40 +0530

    Custom api error  and

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,5 @@
+const user = {
+  username: "idibyaranjan15",
+  loginCount: 8,
+  signedIn: true,
+};
--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,1 +1,14 @@
-const asyncHandler = () => {};
+const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+};
+export default asyncHandler;
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false, // Changed to false to indicate failure
+//       message: error.message,
+//     });
+//   }
+// };
